021b3e1c9ef847c87421bc476227369698e92f01
Dion Amago <dion@transition9.com>
2015-02-12 16:13:23 -0800

    Cookies https://www.npmjs.com/package/cookies

---
+++ js/node/Cookies.hx
@@ -0,0 +1,17 @@
+package js.node;
+
+//Cookies
+//https://www.npmjs.com/package/cookies
+@:native("Cookies")
+extern
+class Cookies
+{
+	public function new(request :NodeHttpServerReq, response :NodeHttpServerResp, ?keys :Dynamic):Void;
+
+	public function get(cookieId :String, ?options :Dynamic) :String;
+	public function set(cookieId :String, ?value :String, ?options :Dynamic) :String;
+	private static function __init__() : Void untyped
+	{
+		var Cookies = untyped __js__("require('cookies')");
+	}
+}
